5172bd00f99a92c0372d6568e8caef63d119753c
Andrew Berry <andy+github@berry.email>
2016-06-27 08:15:28 -0400

    Move product data

--- src/index.js
+++ src/index.js
@@ -1,68 +1,33 @@
 import React from 'react';
 import Inventory from './components/Inventory';
 import Toggle from './components/Toggle';
+import products from './products';
 import { render } from 'react-dom';
 import 'stylesheets/base.scss';
 import 'stylesheets/components/app.scss';
-
-// NOTE: This file is TRASH! Take a look at the specs in each component's __tests__ folder.
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [
-        {
-          sku: 'SK1',
-          name: 'Princess Mini-Figure Set #1',
-          quantity: 12,
-          price: 14.69,
-          image: {
-            src: 'https://images-na.ssl-images-amazon.com/images/I/71mdlG1gj7L._SX522_.jpg'
-          }
-        },
-        {
-          sku: 'SK2',
-          name: 'Princess Small Hand Bag',
-          price: 5.99,
-          quantity: 8,
-          image: {
-            src: 'https://images-na.ssl-images-amazon.com/images/I/71WtXfhsiEL._SX522_.jpg'
-          }
-        },
-        {
-          sku: 'SK3',
-          name: 'Princess Balloons',
-          price: 2.00,
-          quantity: 4,
-          image: {
-            src: 'https://images-na.ssl-images-amazon.com/images/I/61B5z29ySwL._SX522_.jpg'
-          }
-        },
-        {
-          sku: 'SK4',
-          name: 'Frozen Tiara Crown',
-          price: 6.10,
-          quantity: 1,
-          image: {
-            src: 'https://images-na.ssl-images-amazon.com/images/I/71-mKgn-lDL._SX522_.jpg'
-          }
-        }
-      ],
+      products,
       cart: 0
     };
     this.onAddToCart = this.onAddToCart.bind(this);
   }
   onAddToCart(sku) {
-    const products = this.state.products.map((p) => {
-      if (p.sku === sku) {
-        return Object.assign({}, p, { quantity: p.quantity > 0 ? p.quantity - 1 : 0 });
-      }
-      return Object.assign({}, p);
-    });
+    // Add the product to the cart
+    // This simply increments the cart count and decrements the quantity of the
+    // chosen product. Note, IRL use something like redux...
     this.setState({
       cart: this.state.cart + 1,
-      products
+      products: this.state.products.map((p) => {
+        if (p.sku === sku) {
+          // Decrement product quantity
+          return Object.assign({}, p, { quantity: p.quantity > 0 ? p.quantity - 1 : 0 });
+        }
+        return Object.assign({}, p);
+      })
     });
   }
   render() {
---
+++ src/products.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    sku: 'SK1',
+    name: 'Princess Mini-Figure Set #1',
+    quantity: 12,
+    price: 14.69,
+    image: {
+      src: 'https://images-na.ssl-images-amazon.com/images/I/71mdlG1gj7L._SX522_.jpg'
+    }
+  },
+  {
+    sku: 'SK2',
+    name: 'Princess Small Hand Bag',
+    price: 5.99,
+    quantity: 8,
+    image: {
+      src: 'https://images-na.ssl-images-amazon.com/images/I/71WtXfhsiEL._SX522_.jpg'
+    }
+  },
+  {
+    sku: 'SK3',
+    name: 'Princess Balloons',
+    price: 2.00,
+    quantity: 4,
+    image: {
+      src: 'https://images-na.ssl-images-amazon.com/images/I/61B5z29ySwL._SX522_.jpg'
+    }
+  },
+  {
+    sku: 'SK4',
+    name: 'Frozen Tiara Crown',
+    price: 6.10,
+    quantity: 1,
+    image: {
+      src: 'https://images-na.ssl-images-amazon.com/images/I/71-mKgn-lDL._SX522_.jpg'
+    }
+  }
+];
