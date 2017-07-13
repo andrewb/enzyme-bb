92064b44ad60ace8452df3c015f0745fbe6472e4
Andrew Berry <andy+github@berry.email>
2017-07-13 17:32:37 -0400

    Extend component and use onAddToCartHandler

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -3,38 +3,54 @@ import React from 'react';
 import 'stylesheets/components/product.scss';
 import Button from '../Button';
 
-function Product({ sku, name, price, quantity, onAddToCart, image }) {
-  return (
-    <div className="product">
-      {image.src &&
-        <div className="product__image">
-          <img src={image.src} alt={`Product - ${name}`} />
-        </div>
-      }
-      <div className="product__details">
-        <h1 className="product__name">{name}</h1>
-        <div className="product__sku">{sku}</div>
-        <div className="product__price">${price.toFixed(2)}</div>
-        {quantity > 0 &&
-          <div>
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onAddToCartHandler = this.onAddToCartHandler.bind(this);
+  }
+  onAddToCartHandler() {
+    this.props.onAddToCart(this.props.sku);
+  }
+  render() {
+    const {
+      sku,
+      name,
+      price,
+      quantity,
+      image
+    } = this.props;
+    return (
+      <div className="product">
+        {image.src &&
+          <div className="product__image">
+            <img src={image.src} alt={`Product - ${name}`} />
+          </div>
+        }
+        <div className="product__details">
+          <h1 className="product__name">{name}</h1>
+          <div className="product__sku">{sku}</div>
+          <div className="product__price">${price.toFixed(2)}</div>
+          {quantity > 0 &&
+            <div>
+              <div className="product__availability">
+                <span className="product__quantity">{quantity}</span> in Stock
+              </div>
+              <div className="product__cart-button">
+                <Button onClick={this.onAddToCartHandler}>
+                  <span>Add to Cart</span>
+                </Button>
+              </div>
+            </div>
+          }
+          {quantity === 0 &&
             <div className="product__availability">
-              <span className="product__quantity">{quantity}</span> in Stock
+              <div className="product__out-of-stock">Out of Stock!</div>
             </div>
-            <div className="product__cart-button">
-              <Button onClick={() => onAddToCart(sku)}>
-                <span>Add to Cart</span>
-              </Button>
-            </div>
-          </div>
-        }
-        {quantity === 0 &&
-          <div className="product__availability">
-            <div className="product__out-of-stock">Out of Stock!</div>
-          </div>
-        }
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Product.defaultProps = {
