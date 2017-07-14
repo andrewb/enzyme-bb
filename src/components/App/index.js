36384e4c5628635c1040c934025812547d996afc
Andrew Berry <andy+github@berry.email>
2017-07-13 20:49:19 -0400

    Move data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Inventory from '../Inventory';
 import Toggle from '../Toggle';
-import products from '../../products';
+import products from '../../data/products';
 import './app.scss';
 
 class App extends Component {
