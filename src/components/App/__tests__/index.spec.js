572e9d6a0a4be5d873c8340cff56b27629f2d1fc
Andrew Berry <andy+github@berry.email>
2017-07-13 21:02:00 -0400

    Add tests

--- src/components/App/__tests__/index.spec.js
+++ src/components/App/__tests__/index.spec.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../index';
+import Inventory from '../../Inventory';
+import Toggle from '../../Toggle';
 
 describe('<App />', () => {
-  it('renders', () => {
-    shallow(<App />);
+  it('renders an <Inventory />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Inventory).exists()).toBe(true);
+  });
+  it('renders a cart <Toggle />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Toggle).exists()).toBe(true);
   });
 });
